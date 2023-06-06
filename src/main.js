8c56a1b1666177ce3dcb2b26972faade7eba4281
Yang <noriyang0215@gmail.com>
2023-06-06 16:29:22 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+
+import './assets/scss/all.scss'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
